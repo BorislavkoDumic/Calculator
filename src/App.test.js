51123f042873535600fb1b8bc7ba8acf43168415
Borislavko Dumic <borislavko.dumic@biologis.com>
2019-08-28 16:09:53 +0200

    Adding test to projects, fixed some bugs

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import CalculatorContainer from './containers/CalculatorContainers';
-import reducer from './reducers'
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer'
-
-it("Matches the snapshot",()=>{
-  const store = createStore(reducer)
-  const tree = renderer.create(
-  <Provider store= {store}>
-    <CalculatorContainer/>
-  </Provider>).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+import React from "react";
+import CalculatorContainer from "./containers/CalculatorContainers";
+import reducer from "./reducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+describe("Snapshot, container renders correctly", () => {
+  it("Matches the snapshot", () => {
+    const store = createStore(reducer);
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <CalculatorContainer />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -1,56 +1,67 @@
 import * as actionTypes from "../utils/actionTypes";
-import * as operators from "../utils/actionTypes"
+import * as operators from "../utils/actionTypes";
 
-export const initialState ={
-  displayValue:'0',
-  operator:null,
-  previusNumber:0,
+export const initialState = {
+  displayValue: "0",
+  operator: null,
+  previusNumber: 0
 };
 
-export const calculate = (displayValue, previusNumber,operator)=>{
-    switch(operator){
-        case operators.PLUS:
-            return previusNumber + displayValue;
-        case operators.SUBTRACT:
-            return previusNumber - displayValue;
-        case operators.MULTI:
-            return previusNumber * displayValue;
-        case operators.DIVIDE:
-            return previusNumber / displayValue;
-            default:
-                return displayValue;
-    }
-
+export const calculate = (displayValue, previusNumber, operator) => {
+  switch (operator) {
+    case operators.PLUS:
+      return previusNumber + displayValue;
+    case operators.SUBTRACT:
+      return previusNumber - displayValue;
+    case operators.MULTI:
+      return previusNumber * displayValue;
+    case operators.DIVIDE:
+      return previusNumber / displayValue;
+    default:
+      return displayValue;
+  }
 };
-const calculator = (state = initialState, action ) =>{
-    switch (action.type){
-        case actionTypes.INPUT_NUMBER:
-            return {
-                ...state,
-                displayValue: state.displayValue === "0" ? action.number.toString() :`${ state.displayValue }${ action.number }`
-            };
-        case actionTypes.INPUT_OPERATOR:
-            return {
-                displayValue:'0',
-                operator:action.operator,
-                previusNumber:state.operator ? calculate ( parseFloat( state.displayValue ), state.previusNumber, state.operator ): parseFloat( state.displayValue )
-            };
-        case actionTypes.INPUT_EQUAL:
-            return { 
-                displayValue: calculate( parseFloat( state.displayValue ), state.previusNumber, state.operator ).toString(),
-                operator: null,
-                previusNumber: 0
-            };
-        case actionTypes.CLEAR:
-            return {
-               displayValue:"0",
-               operator:null,
-               previusNumber:0,
-            };
-        default:
-            return state;   
-
-    }
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.INPUT_NUMBER:
+      return {
+        ...state,
+        displayValue:
+          state.displayValue === "0"
+            ? action.number.toString()
+            : `${state.displayValue}${action.number}`
+      };
+    case actionTypes.INPUT_OPERATOR:
+      return {
+        displayValue: "0",
+        operator: action.operator,
+        previusNumber: state.operator
+          ? calculate(
+              parseFloat(state.displayValue),
+              state.previusNumber,
+              state.operator
+            )
+          : parseFloat(state.displayValue)
+      };
+    case actionTypes.INPUT_EQUAL:
+      return {
+        displayValue: calculate(
+          parseFloat(state.displayValue),
+          state.previusNumber,
+          state.operator
+        ).toString(),
+        operator: null,
+        previusNumber: 0
+      };
+    case actionTypes.CLEAR:
+      return {
+        displayValue: "0",
+        operator: null,
+        previusNumber: 0
+      };
+    default:
+      return state;
+  }
 };
 
 export default calculator;
